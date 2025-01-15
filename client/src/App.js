a72b9b401e18b3c34e3f3adfa56915cf08984eae
birinder singh <birinder229.be22@chitkara.edu.in>
2025-01-16 00:23:30 +0530

    authentication completed without css

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,26 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import {StreamChat} from 'stream-chat'
 import Cookies from "universal-cookie"
+import { useState } from 'react';
 
 function App() {
   const api_key = "8qhycckxhwfj"
   const client = StreamChat.getInstance( api_key)
   const cookies = new Cookies()
   const token = cookies.get("token")
-  
+  const [isAuth, setIsAuth] = useState(false);
+
+  const logOut = () => {
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("firstName");
+    cookies.remove("lastName");
+    cookies.remove("hashedPassword");
+    cookies.remove("channelName");
+    cookies.remove("username");
+    client.disconnectUser();
+    setIsAuth(false);
+  }
   if(token){
     client.connectUser({
       id: cookies.get("userId"),
@@ -20,13 +33,19 @@ function App() {
     },
     token
     ).then((user) => {
-      console.log(user);
+      setIsAuth(true);
     })
   }
   return (
     <div className="App">
-      <Signup />
-      <Login />
+      {isAuth ? (
+        <button onClick={logOut}> Log Out</button>
+      ) : (
+        <>
+          <Signup setIsAuth={setIsAuth} />
+          <Login setIsAuth={setIsAuth}/>
+        </>
+      )}
     </div>
   );
 }
